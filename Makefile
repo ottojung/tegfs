40b3d1384bdd869fd3e8dd6a03f4482a99f0f0d8
Otto Jung <otto.jung@kakao.com>
2022-02-03 13:47:37 +0100

    parse arguments

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 
-test1:
-	echo hi | czempak run src/add.scm hello
+TEST_ROOT=build/testroot
 
+test1: build/add
+	echo hi | TEGFS_ROOT=$(TEST_ROOT) build/add --registry-file testreg.tegfs.org
+
+build/add: src/add.scm build
+	czempak install src/add.scm "$@"
+
+build:
+	mkdir -p "$@"
--- src/add.scm
+++ src/add.scm
@@ -8,19 +8,66 @@
 %use (alphanum/alphabet) "./euphrates/alphanum-alphabet.scm"
 %use (make-directories) "./euphrates/make-directories.scm"
 %use (read-all-port) "./euphrates/read-all-port.scm"
+%use (dprintln) "./euphrates/dprintln.scm"
+%use (current-program-path/p) "./euphrates/current-program-path-p.scm"
+%use (file-or-directory-exists?) "./euphrates/file-or-directory-exists-q.scm"
+%use (write-string-file) "./euphrates/write-string-file.scm"
+%use (append-string-file) "./euphrates/append-string-file.scm"
 
 %use (debug) "./euphrates/debug.scm"
 
-;; (with-cli
-;;  (MAIN
-;;   MAIN : OPT* <file...>+
-;;   /     --help
-;;   OPT : --force
-;;   /     --recursive
-;;   /     -fr
-;;   /     -rf
-;;   /     -v
-;;   )
+(define (main)
 
-(debug "READ: ~s" (read-all-port (current-input-port)))
+  (define (fatal fmt . args)
+    (parameterize ((current-output-port (current-error-port)))
+      (apply dprintln (cons fmt args)))
+    (exit 1))
 
+  (parameterize ((current-program-path/p "tegfs-add"))
+    (with-cli
+     (MAIN
+      MAIN : OPT*
+      OPT : KEY
+      /     TITLE
+      /     TAG
+      /     REGISTRY
+      /     ROOT
+      KEY : --key <key...> <value...>
+      TITLE : --title <title>
+      TAG : --tag <tag...>
+      REGISTRY : --registry-file <registry-file>
+      ROOT : --root <root>
+      )
+
+     (define ROOT_VAR_NAME "TEGFS_ROOT")
+
+     (define root
+       (or <root>
+           (system-environment-get ROOT_VAR_NAME)))
+
+     (unless root
+       (fatal "Cannot cd into root because ~s env variable is not defined" ROOT_VAR_NAME))
+
+     (unless <registry-file>
+       (fatal "Parameter <registry-file> is required, but it is not set"))
+
+     (unless (file-or-directory-exists? root)
+       (make-directories root))
+
+     (chdir root)
+
+     (unless (file-or-directory-exists? <registry-file>)
+       (write-string-file
+        <registry-file>
+        "# This file was automatically created by tegfs-add\n\n\n"))
+
+     (debug "HI")
+
+     (define input
+       (read-all-port (current-input-port)))
+
+     (debug "input: ~s" input)))
+
+  (dprintln "All done"))
+
+(main)
