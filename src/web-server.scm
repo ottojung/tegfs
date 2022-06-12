4cabd8453dcc753952cb204e039cdfb56d4cdfb1
Otto Jung <otto.jung@kakao.com>
2022-06-12 14:26:07 +0200

    web-server: abstract unavailable-string

--- src/web-server.scm
+++ src/web-server.scm
@@ -61,6 +61,7 @@
 %use (list-singleton?) "./euphrates/list-singleton-q.scm"
 %use (absolute-posix-path?) "./euphrates/absolute-posix-path-q.scm"
 %use (get-current-directory) "./euphrates/get-current-directory.scm"
+%use (stringf) "./euphrates/stringf.scm"
 
 %use (root/p) "./root-p.scm"
 %use (categorization-filename) "./categorization-filename.scm"
@@ -554,13 +555,16 @@
        (let* ((registry-dir (dirname (cdr (assoc entry-registry-path-key entry)))))
          (append-posix-path (root/p) registry-dir (cdr target-p)))))
 
-(define unavailable-string
-  "<?xml version='1.0' encoding='UTF-8' standalone='no'?>
-   <!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
-   <svg width='400px' height='225px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M18.364 5.63604C21.8787 9.15076 21.8787 14.8492 18.364 18.364C14.8492 21.8787 9.15076 21.8787 5.63604 18.364C2.12132 14.8492 2.12132 9.15076 5.63604 5.63604C9.15076 2.12132 14.8492 2.12132 18.364 5.63604ZM16.1925 17.6067L6.39327 7.80749C4.33767 10.5493 4.55666 14.4562 7.05025 16.9497C9.54384 19.4433 13.4507 19.6623 16.1925 17.6067ZM16.9497 7.05025C19.4433 9.54384 19.6623 13.4507 17.6067 16.1925L7.80749 6.39327C10.5493 4.33767 14.4562 4.55666 16.9497 7.05025Z' fill='black'/></svg>")
+(define unavailable-image-string
+  (stringf
+   "<?xml version='1.0' encoding='UTF-8' standalone='no'?>
+    <!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
+    <svg width='~apx' height='~apx' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M18.364 5.63604C21.8787 9.15076 21.8787 14.8492 18.364 18.364C14.8492 21.8787 9.15076 21.8787 5.63604 18.364C2.12132 14.8492 2.12132 9.15076 5.63604 5.63604C9.15076 2.12132 14.8492 2.12132 18.364 5.63604ZM16.1925 17.6067L6.39327 7.80749C4.33767 10.5493 4.55666 14.4562 7.05025 16.9497C9.54384 19.4433 13.4507 19.6623 16.1925 17.6067ZM16.9497 7.05025C19.4433 9.54384 19.6623 13.4507 17.6067 16.1925L7.80749 6.39327C10.5493 4.33767 14.4562 4.55666 16.9497 7.05025Z' fill='black'/></svg>"
+   web-preview-width web-preview-height
+   ))
 
 (define unavailable-bytevector
-  (string->utf8 unavailable-string))
+  (string->utf8 unavailable-image-string))
 
 (define (preview-unavailable)
   (return!
