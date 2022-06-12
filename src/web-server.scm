4d8c65c38ea78e63b56e6b93a867da7be3b302b1
Otto Jung <otto.jung@kakao.com>
2022-06-12 11:55:32 +0200

    web-server: show unavailable logo when preview cannot be generated

--- src/web-server.scm
+++ src/web-server.scm
@@ -440,7 +440,7 @@
    ((file-is-image? target-fullpath)
     (generate-preview-internet-path target-id))
    (else
-    'TODO)))
+    (generate-preview-internet-path "unavailable"))))
 
 (define (display-preview preview-fullpath)
   (display "<img src=")
@@ -554,18 +554,40 @@
        (let* ((registry-dir (dirname (cdr (assoc entry-registry-path-key entry)))))
          (append-posix-path (root/p) registry-dir (cdr target-p)))))
 
-(define (preview)
-  (define callctx (callcontext/p))
-  (define request (callcontext-request callctx))
-  (define ctxq (callcontext-query callctx))
+(define unavailable-string
+  "<?xml version='1.0' encoding='UTF-8' standalone='no'?>
+   <!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
+   <svg width='400px' height='225px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M18.364 5.63604C21.8787 9.15076 21.8787 14.8492 18.364 18.364C14.8492 21.8787 9.15076 21.8787 5.63604 18.364C2.12132 14.8492 2.12132 9.15076 5.63604 5.63604C9.15076 2.12132 14.8492 2.12132 18.364 5.63604ZM16.1925 17.6067L6.39327 7.80749C4.33767 10.5493 4.55666 14.4562 7.05025 16.9497C9.54384 19.4433 13.4507 19.6623 16.1925 17.6067ZM16.9497 7.05025C19.4433 9.54384 19.6623 13.4507 17.6067 16.1925L7.80749 6.39327C10.5493 4.33767 14.4562 4.55666 16.9497 7.05025Z' fill='black'/></svg>")
 
-  (define target-id (hashmap-ref ctxq 't #f))
+(define unavailable-bytevector
+  (string->utf8 unavailable-string))
+
+(define (preview-unavailable)
+  (return!
+   (build-response
+    #:code 200
+    #:headers
+    (append web-basic-headers
+            `((content-type . (image/svg+xml))
+              (Cache-Control . "max-age=3600, public, private"))))
+   unavailable-bytevector))
+
+(define (preview-available target-id)
   (define entry (tegfs-get/cached target-id))
   (define target-fullpath (entry-target-fullpath entry))
   (define preview-fullpath
     (web-make-preview target-id target-fullpath entry))
 
   (web-sendfile return! 'image/jpeg preview-fullpath))
+
+(define (preview)
+  (define callctx (callcontext/p))
+  (define request (callcontext-request callctx))
+  (define ctxq (callcontext-query callctx))
+  (define target-id (hashmap-ref ctxq 't #f))
+  (if (equal? target-id "unavailable")
+      (preview-unavailable)
+      (preview-available target-id)))
 
 (define (full)
   (define ctx (context/p))
