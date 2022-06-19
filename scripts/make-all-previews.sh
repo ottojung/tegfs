4993d934075d2a137e9af7fe97f6839f45be03e5
Otto Jung <otto.jung@vauplace.com>
2022-06-19 14:03:32 +0200

    scripts: disable preview maker counter

--- scripts/make-all-previews.sh
+++ scripts/make-all-previews.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-COUNTER=0
-
 tegfs query --format '%F "#" %P' image | grep -v '//NA//' | sort --reverse | while IFS= read -r FILE
 do
 	TARGET="$(echo "$FILE" | awk -F '#' '{ print $1 }')"
@@ -10,7 +8,4 @@ do
 	test -f "$PREVIEW" && continue
 	echo '>' tegfs make-thumbnails "$TARGET"
 	tegfs make-thumbnails "$TARGET" "$PREVIEW"
-	COUNTER=$((COUNTER + 1))
 done
-
-echo "$COUNTER previews made" 1>&2
