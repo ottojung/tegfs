c939b917d31ea31f8d570510fbb4d4937b415f11
Otto Jung <otto.jung@vauplace.com>
2022-06-19 12:22:08 +0200

    query: really handle the case

--- src/prolog.scm
+++ src/prolog.scm
@@ -113,14 +113,15 @@
 ;;
 (define (tegfs-dump-prolog)
   (display ":-style_check(-discontiguous).\n")
-  (translate-registries yield-for-prolog)
-  (dump-rules)
-  (display "v(_, _) :- false.\n")
-  (display "t('%diff', [X, Y]) :- X \\= Y.\n")
-  (display "t('%any', _).\n")
-  (display "vandthis(This, X) :- X = This ; v(This, X).\n")
-  ;; (display "what(X, Y) :- t(Y, X) ; t(K, Z), member(X, Z), Y = [K | Z].") (newline)
-  )
+  (let ((ret
+         (translate-registries yield-for-prolog)))
+    (dump-rules)
+    (display "v(_, _) :- false.\n")
+    (display "t('%diff', [X, Y]) :- X \\= Y.\n")
+    (display "t('%any', _).\n")
+    (display "vandthis(This, X) :- X = This ; v(This, X).\n")
+    ;; (display "what(X, Y) :- t(Y, X) ; t(K, Z), member(X, Z), Y = [K | Z].") (newline)
+    ret))
 
 (define (tegfs-dump-prolog-file)
   (define output-path (string-append (make-temporary-filename) ".pl"))
@@ -154,7 +155,9 @@
   (for-each
    (lambda (tag)
      (yield `(tag ,tag)))
-   (hashset->list tags-set)))
+   (hashset->list tags-set))
+
+  (< 1 (counter)))
 
 (define (alpha-convert-variable cnt)
   (lambda (name)
