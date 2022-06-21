4ef66476b182b1226ffe65d225068ba8bd227b54
Otto Jung <otto.jung@vauplace.com>
2022-06-21 19:47:10 +0200

    scripts: change separator in query output to avoid colisions

--- scripts/make-all-previews.sh
+++ scripts/make-all-previews.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 
-tegfs query --format '%F "#" %P' %any | grep -v '//NA//' | sort --reverse | while IFS= read -r FILE
+tegfs query --format '%F "#tegfs-separator#" %P' %any | grep -v '//NA//' | sort --reverse | while IFS= read -r FILE
 do
-	TARGET="$(echo "$FILE" | awk -F '#' '{ print $1 }')"
+	TARGET="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $1 }')"
 	case "$TARGET" in
 		 http://*) ;;
 		 https://*) ;;
 		 *) test -f "$TARGET" || continue ;;
 	esac
 
-	PREVIEW="$(echo "$FILE" | awk -F '#' '{ print $2 }')"
+	PREVIEW="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $2 }')"
 	test -f "$PREVIEW" && continue
 	echo '>' tegfs make-thumbnails "$TARGET"
 	tegfs make-thumbnails "$TARGET" "$PREVIEW" || touch "$PREVIEW"
