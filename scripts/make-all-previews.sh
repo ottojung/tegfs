a54a1bdc90ff4451203660eb30d06dcc094dd892
Otto Jung <otto.jung@vauplace.com>
2022-07-10 19:58:08 +0200

    scripts: dont check if file exists

--- scripts/make-all-previews.sh
+++ scripts/make-all-previews.sh
@@ -2,16 +2,12 @@
 
 tegfs list --dirs --format '%F "#tegfs-separator#" %P' | grep -v '//NA//' | while IFS= read -r FILE
 do
-	TARGET="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $1 }')"
-	case "$TARGET" in
-		 http://*) ;;
-		 https://*) ;;
-		 *) test -f "$TARGET" || continue ;;
-	esac
-
 	PREVIEW="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $2 }')"
 	test -f "$PREVIEW" && continue
 	test -e "$TEGFS_ROOT/cache/failed-previews/$PREVIEW" && continue
+	TARGET="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $1 }')"
 	echo '>' tegfs make-thumbnails "$TARGET"
 	tegfs make-thumbnails "$TARGET" "$PREVIEW" || mkdir -p "$TEGFS_ROOT/cache/failed-previews/$PREVIEW"
 done
+
+
