91f8132b204bb7ad02a21067fb6e55b083561aac
Otto Jung <otto.jung@vauplace.com>
2022-07-10 19:48:10 +0200

    scripts: dont sort previews list

--- scripts/make-all-previews.sh
+++ scripts/make-all-previews.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-tegfs list --dirs --format '%F "#tegfs-separator#" %P' | grep -v '//NA//' | sort --reverse | while IFS= read -r FILE
+tegfs list --dirs --format '%F "#tegfs-separator#" %P' | grep -v '//NA//' | while IFS= read -r FILE
 do
 	TARGET="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $1 }')"
 	case "$TARGET" in
