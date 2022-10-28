78d9b2c700682496022ca8ab54f9ef435363c230
Otto Jung <otto.jung@vauplace.com>
2022-10-28 21:47:45 +0200

    web: factor out filemap

---
+++ src/filemap.scm
@@ -0,0 +1,58 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; version 3 of the License.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%run guile
+
+%var filemap-set!
+%var filemap-ref-by-vid
+%var filemap-ref-by-sharedname
+%var filemap-delete-by-vid!
+%var filemap-delete-by-sharedname!
+
+%use (hashmap-delete! hashmap-ref hashmap-set!) "./euphrates/ihashmap.scm"
+%use (sharedinfo-sharedname sharedinfo-vid) "./web-sharedinfo.scm"
+
+(define (filemap-set! filemap/2 info)
+  (define vid (sharedinfo-vid info))
+  (define sharedname (sharedinfo-sharedname info))
+  (define first (car filemap/2))
+  (define second (cdr filemap/2))
+  (hashmap-set! first vid info)
+  (hashmap-set! second sharedname info))
+
+(define (filemap-ref-by-vid filemap/2 id default)
+  (define first (car filemap/2))
+  (hashmap-ref first id default))
+
+(define (filemap-ref-by-sharedname filemap/2 sharedname default)
+  (define second (cdr filemap/2))
+  (hashmap-ref second sharedname default))
+
+(define (filemap-delete-by-vid! filemap/2 id)
+  (define first (car filemap/2))
+  (define second (cdr filemap/2))
+  (define info (filemap-ref-by-vid filemap/2 id #f))
+  (when info
+    (let ((sharedname (sharedinfo-sharedname info)))
+      (hashmap-delete! first id)
+      (hashmap-delete! second sharedname))))
+
+(define (filemap-delete-by-sharedname! filemap/2 sharedname)
+  (define first (car filemap/2))
+  (define second (cdr filemap/2))
+  (define info (filemap-ref-by-sharedname filemap/2 sharedname #f))
+  (when info
+    (let ((id (sharedinfo-vid info)))
+      (hashmap-delete! first id)
+      (hashmap-delete! second sharedname))))
--- src/web-server.scm
+++ src/web-server.scm
@@ -54,6 +54,7 @@
 %use (tegfs-process-categorization-text) "./edit-tags.scm"
 %use (entry-parent-directory-vid-key) "./entry-parent-directory-vid-key.scm"
 %use (entry-target-fullpath) "./entry-target-fullpath.scm"
+%use (filemap-delete-by-sharedname! filemap-ref-by-sharedname filemap-ref-by-vid filemap-set!) "./filemap.scm"
 %use (get-config) "./get-config.scm"
 %use (get-preview-path) "./get-preview-path.scm"
 %use (get-random-basename) "./get-random-basename.scm"
@@ -124,40 +125,6 @@
   (define now (callcontext-time callctx))
   (define vid (get-random-network-name))
   (sharedinfo-ctr target-fullpath sharedname vid token now for-duration))
-
-(define (filemap-set! filemap/2 info)
-  (define vid (sharedinfo-vid info))
-  (define sharedname (sharedinfo-sharedname info))
-  (define first (car filemap/2))
-  (define second (cdr filemap/2))
-  (hashmap-set! first vid info)
-  (hashmap-set! second sharedname info))
-
-(define (filemap-ref-by-vid filemap/2 id default)
-  (define first (car filemap/2))
-  (hashmap-ref first id default))
-
-(define (filemap-ref-by-sharedname filemap/2 sharedname default)
-  (define second (cdr filemap/2))
-  (hashmap-ref second sharedname default))
-
-(define (filemap-delete-by-vid! filemap/2 id)
-  (define first (car filemap/2))
-  (define second (cdr filemap/2))
-  (define info (filemap-ref-by-vid filemap/2 id #f))
-  (when info
-    (let ((sharedname (sharedinfo-sharedname info)))
-      (hashmap-delete! first id)
-      (hashmap-delete! second sharedname))))
-
-(define (filemap-delete-by-sharedname! filemap/2 sharedname)
-  (define first (car filemap/2))
-  (define second (cdr filemap/2))
-  (define info (filemap-ref-by-sharedname filemap/2 sharedname #f))
-  (when info
-    (let ((id (sharedinfo-vid info)))
-      (hashmap-delete! first id)
-      (hashmap-delete! second sharedname))))
 
 (define (return! stats body)
   (define callctx (web-callcontext/p))
