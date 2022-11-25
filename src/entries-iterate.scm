385322d5d36e29fb32bcff653fb00a959a0dc892
Otto Jung <otto.jung@vauplace.com>
2022-11-25 01:46:05 +0100

    add entries-iterate

---
+++ src/entries-iterate.scm
@@ -0,0 +1,62 @@
+;;;; Copyright (C) 2022  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU Affero General Public License as published
+;;;; by the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU Affero General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Affero General Public License
+;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+%run guile
+
+%var entries-iterate
+
+%use (append-posix-path) "./euphrates/append-posix-path.scm"
+%use (open-file-port) "./euphrates/open-file-port.scm"
+%use (list->stack stack-empty? stack-pop!) "./euphrates/stack.scm"
+%use (get-registry-files) "./get-registry-files.scm"
+%use (get-root) "./get-root.scm"
+%use (keyword-entry-registry-path) "./keyword-entry-registry-path.scm"
+
+;; Iterator returns #f when done.
+(define (entries-iterate)
+  (define registries
+    (list->stack
+     (get-registry-files)))
+
+  (define input-port #f)
+  (define registry-property #f)
+
+  (define (init-next-registry)
+    (define registry-path
+      (stack-pop! registries))
+    (define registry-fullpath
+      (append-posix-path (get-root) registry-path))
+
+    (set! registry-property
+          (cons keyword-entry-registry-path registry-path))
+    (set! input-port
+          (open-file-port registry-fullpath "r")))
+
+  (define (next)
+    (if input-port
+        (let ((x (read input-port)))
+          (if (eof-object? x)
+              (begin
+                (close-port input-port)
+                (set! input-port #f)
+                (next))
+              (cons registry-property x)))
+        (if (stack-empty? registries)
+            #f
+            (begin
+              (init-next-registry)
+              (next)))))
+
+  next)
