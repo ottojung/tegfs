d8d36ed7608831cb2e2c28ac66dcdac510c7f087
Otto Jung <otto.jung@vauplace.com>
2022-12-04 02:16:22 +0100

    CLI-query: use profune-communications

--- src/CLI-query.scm
+++ src/CLI-query.scm
@@ -17,46 +17,55 @@
 
 %var CLI-query
 
+%use (assq-or) "./euphrates/assq-or.scm"
 %use (comp) "./euphrates/comp.scm"
-%use (monad-make/hook) "./euphrates/monad-make-hook.scm"
 %use (printf) "./euphrates/printf.scm"
-%use (with-monad) "./euphrates/with-monad.scm"
+%use (profun-create-database) "./euphrates/profun.scm"
+%use (make-profune-communicator profune-communicator-handle) "./euphrates/profune-communicator.scm"
 %use (entry-print/formatted) "./entry-print-formatted.scm"
 %use (entry-print) "./entry-print.scm"
 %use (fatal) "./fatal.scm"
 %use (get-admin-permissions) "./get-admin-permissions.scm"
-%use (tegfs-query) "./tegfs-query.scm"
+%use (tegfs-server-handler) "./tegfs-server-handler.scm"
 
 (define (CLI-query --diropen --dirpreview --entries <query-format> <query...>)
-  (define counter 0)
   (define print-func
     (cond
      (--entries (lambda (entry) (entry-print entry) (newline)))
      (<query-format> (comp (entry-print/formatted <query-format>)))
      (else (fatal "Unexpected mode: both --entries and <query-format> were not set"))))
-  (define show-monad
-    (monad-make/hook
-     (lambda (tags)
-       (cond
-        ((memq 'unfold-entry tags) (lambda (f) (f)))
-        ((memq 'entry tags)
-         (lambda (entry)
-           (set! counter (+ 1 counter))
-           (print-func entry)
-           (newline)))
-        ((memq 'ask tags)
-         (lambda (type)
-           (case type
-             ((query/split) <query...>)
-             ((filemap/2) #f) ;; TODO: get from server
-             ((permissions) (get-admin-permissions)) ;; TODO: get from command-line & server
-             ((diropen?) --diropen)
-             ((dirpreview?) --dirpreview))))))))
 
-  (with-monad show-monad (tegfs-query))
+  (define db
+    (profun-create-database
+     tegfs-server-handler
+     '()))
+
+  (define comm
+    (make-profune-communicator db))
+
+  (define result
+    (profune-communicator-handle
+     comm
+     `(whats
+       (permissions ,(get-admin-permissions))
+       (query ,<query...>)
+       (entry E)
+       more (99999)
+       )))
+
+  (define equals (cadr (cadr result)))
+
+  (for-each
+   (lambda (bindings)
+     (define entry
+       (assq-or 'E bindings (fatal "Unexpected result from backend")))
+     (print-func entry)
+     (newline))
+   equals)
 
   (parameterize ((current-output-port (current-error-port)))
-    (if (equal? 0 counter)
-        (display "No matches.")
-        (printf "Total of ~a matches." counter))
-    (newline)))
+    (let ((counter (length equals)))
+      (if (equal? 0 counter)
+          (display "No matches.")
+          (printf "Total of ~a matches." counter))
+      (newline))))
