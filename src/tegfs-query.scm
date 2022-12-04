862c47a39b31d97e1b0cf6c8677b798708414983
Otto Jung <otto.jung@vauplace.com>
2022-12-04 13:15:46 +0100

    remove monads alltogether

--- src/tegfs-query.scm
+++ src/tegfs-query.scm
@@ -15,22 +15,16 @@
 
 %run guile
 
-%var tegfs-query
 %var query-entry-handler
 
 %use (appcomp comp) "./euphrates/comp.scm"
 %use (curry-if) "./euphrates/curry-if.scm"
-%use (monad-ask) "./euphrates/monad-ask.scm"
-%use (monad-do) "./euphrates/monad-do.scm"
 %use (profun-accept profun-ctx-set profun-set) "./euphrates/profun-accept.scm"
 %use (make-profun-error) "./euphrates/profun-error.scm"
 %use (profun-op-envlambda) "./euphrates/profun-op-envlambda.scm"
 %use (profun-reject) "./euphrates/profun-reject.scm"
 %use (profun-bound-value? profun-unbound-value?) "./euphrates/profun-value.scm"
 %use (has-access-for-entry-details? has-access-for-entry-target?) "./access.scm"
-%use (default-preview-sharing-time) "./default-preview-sharing-time.scm"
-%use (entry-target-fullpath) "./entry-target-fullpath.scm"
-%use (get-preview-path) "./get-preview-path.scm"
 %use (keyword-diropen) "./keyword-diropen.scm"
 %use (keyword-dirpreview) "./keyword-dirpreview.scm"
 %use (keyword-entry-parent-directory) "./keyword-entry-parent-directory.scm"
@@ -39,7 +33,6 @@
 %use (keyword-title) "./keyword-title.scm"
 %use (query-diropen?/p query-dirpreview?/p query-filemap/2/p query-permissions/p query-split/p) "./talk-parameters.scm"
 %use (tegfs-query/open) "./tegfs-query-open.scm"
-%use (web-share-file) "./web-share-file.scm"
 
 (define-syntax profun-default
   (syntax-rules ()
@@ -100,69 +93,6 @@
               (ret iter))
 
           )))))
-
-;; Monad contract:
-;; - type { 'ask }
-;;   where type is:
-;;   - 'query/split (REQUIRED)
-;;   - 'permissions  (REQUIRED)
-;;   - 'filemap/2  (REQUIRED)
-;;   - 'diropen? (DEFAULT #f)
-;;   - 'dirpreview? (DEFAULT #f)
-;; - unfold-entry { 'unfold-entry, 'say, 'many } (OPTIONAL)
-;;   unfolds to:
-;;   - entry { 'entry, 'say } (OPTIONAL)
-;;   - share-full { 'share-full, 'say } (OPTIONAL)
-;;   - share-preview { 'share-preview, 'say } (OPTIONAL)
-
-(define (tegfs-query)
-  (monad-ask query/split)
-  (monad-ask permissions)
-  (monad-ask filemap/2)
-  (monad-ask diropen? :default #f)
-  (monad-ask dirpreview? :default #f)
-
-  (define opening-properties
-    (appcomp
-     '()
-     ((curry-if (const diropen?) (comp (cons keyword-diropen))))
-     ((curry-if (const dirpreview?) (comp (cons keyword-dirpreview))))))
-
-  (define (for-each-fn entry0)
-    (define entry
-      (cond
-       ((has-access-for-entry-details? filemap/2 permissions entry0)
-        (lambda _ entry0))
-       ((has-access-for-entry-target? filemap/2 permissions entry0)
-        (lambda _
-          (filter (lambda (p) (memq (car p) target-fields)) entry0)))
-       (else #f)))
-
-    (define (share-full)
-      0)
-
-    (define (share-preview)
-      (define target-fullpath (entry-target-fullpath entry0))
-      (define preview-fullpath (get-preview-path target-fullpath))
-      (web-share-file preview-fullpath default-preview-sharing-time))
-
-    (define (unfold-entry)
-      (monad-do (entry) 'entry 'say)
-      (monad-do (share-full) 'share-full 'say)
-      (monad-do (share-preview) 'share-preview 'say)
-      )
-
-    (when entry
-      (monad-do unfold-entry 'unfold-entry 'say 'many)))
-
-  (define iter
-    (tegfs-query/open opening-properties query/split))
-
-  (let loop ()
-    (define x (iter))
-    (when x
-      (for-each-fn x)
-      (loop))))
 
 (define target-fields
   (list keyword-target
