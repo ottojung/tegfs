21633ac298b17af2ca11af83acd14b7aa8f4f98a
Otto Jung <otto.jung@vauplace.com>
2022-12-16 13:34:09 +0100

    [BROKEN] web-get-full-link: dont demand much

--- src/web-get-full-link.scm
+++ src/web-get-full-link.scm
@@ -19,29 +19,31 @@
 
 %use (append-posix-path) "./euphrates/append-posix-path.scm"
 %use (assoc-or) "./euphrates/assoc-or.scm"
+%use (assq-or) "./euphrates/assq-or.scm"
 %use (file-is-directory?/no-readlink) "./euphrates/file-is-directory-q-no-readlink.scm"
 %use (raisu) "./euphrates/raisu.scm"
 %use (stringf) "./euphrates/stringf.scm"
 %use (uri-encode) "./euphrates/uri-encode.scm"
 %use (a-weblink?) "./a-weblink-q.scm"
-%use (default-full-sharing-time) "./default-full-sharing-time.scm"
 %use (entry-for-local-file?) "./entry-for-local-file-huh.scm"
+%use (entry-target-fullpath) "./entry-target-fullpath.scm"
 %use (filemap-ref-by-senderid) "./filemap.scm"
 %use (keyword-entry-parent-directory-senderid) "./keyword-entry-parent-directory-senderid.scm"
 %use (keyword-target) "./keyword-target.scm"
-%use (sharedinfo-recepientid sharedinfo-senderid) "./sharedinfo.scm"
+%use (sharedinfo-recepientid) "./sharedinfo.scm"
 %use (web-context/p) "./web-context-p.scm"
 %use (context-filemap/2 context-fileserver) "./web-context.scm"
-%use (web-get-permissions) "./web-get-permissions.scm"
-%use (web-share-file/dont-link-yet) "./web-share-file.scm"
 
-(define (web-get-full-link entry target-fullpath)
+(define (web-get-full-link entry maybe-senderid)
+  (define target (assq-or keyword-target entry #f))
   (cond
-   ((a-weblink? target-fullpath)
-    target-fullpath)
+   (maybe-senderid (string-append "/full?vid=" maybe-senderid))
+   ((a-weblink? target) target)
    ((entry-for-local-file? entry)
-    (let* ((parent-senderid (or (assoc-or keyword-entry-parent-directory-senderid entry #f)
-                           (raisu 'entry-does-not-have-parent-senderid entry)))
+    (let* ((target-fullpath (or (entry-target-fullpath entry)
+                                (raisu 'local-file-entry-does-not-have-target-info entry)))
+           (parent-senderid (or (assoc-or keyword-entry-parent-directory-senderid entry #f)
+                                (raisu 'entry-does-not-have-parent-senderid entry)))
            (ctx (web-context/p))
            (filemap/2 (context-filemap/2 ctx))
            (info (or (filemap-ref-by-senderid filemap/2 parent-senderid #f)
@@ -56,9 +58,4 @@
                  (recepientid (sharedinfo-recepientid info)))
             (append-posix-path fileserver recepientid suffix)))))
    (else
-    (let* ((ctx (web-context/p))
-           (perm (web-get-permissions))
-           (info (web-share-file/dont-link-yet ctx perm target-fullpath default-full-sharing-time))
-           (vid (and info (sharedinfo-senderid info)))
-           (location (and info (string-append "/full?vid=" vid))))
-      (and info location)))))
+    (raisu 'not-enough-information-to-get-full-link entry))))
