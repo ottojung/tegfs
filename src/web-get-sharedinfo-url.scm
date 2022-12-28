2670925704ddccd75ec4a976f64b7fec6f12edf2
Otto Jung <otto.jung@vauplace.com>
2022-12-28 18:49:06 +0100

    web: fix bad full links

--- src/web-get-sharedinfo-url.scm
+++ src/web-get-sharedinfo-url.scm
@@ -19,8 +19,11 @@
 
 %use (append-posix-path) "./euphrates/append-posix-path.scm"
 %use (file-is-directory?/no-readlink) "./euphrates/file-is-directory-q-no-readlink.scm"
+%use (list-intersperse) "./euphrates/list-intersperse.scm"
 %use (path-normalize) "./euphrates/path-normalize.scm"
 %use (remove-common-prefix) "./euphrates/remove-common-prefix.scm"
+%use (string-split/simple) "./euphrates/string-split-simple.scm"
+%use (uri-encode) "./euphrates/uri-encode.scm"
 %use (a-weblink?) "./a-weblink-q.scm"
 %use (sharedinfo-recepientid sharedinfo-senderid sharedinfo-sourcepath) "./sharedinfo.scm"
 %use (context-fileserver) "./web-context.scm"
@@ -38,13 +41,20 @@
    ((file-is-directory?/no-readlink target-fullpath)
     (string-append "/directory?vid=" vid))
    (container-info
-    (let ((relative-path
-           (path-normalize
-            (string-append
-             (sharedinfo-recepientid container-info)
-             "/"
-             (let ((container-path (sharedinfo-sourcepath container-info)))
-               (remove-common-prefix target-fullpath container-path))))))
+    (let* ((container-path (sharedinfo-sourcepath container-info))
+           (suffix (remove-common-prefix target-fullpath container-path))
+           (escaped-suffix
+            (apply
+             string-append
+             (list-intersperse
+              "/"
+              (map uri-encode
+                   (string-split/simple suffix #\/)))))
+           (relative-path
+            (path-normalize
+             (string-append
+              (sharedinfo-recepientid container-info)
+              "/" escaped-suffix))))
       (append-posix-path fileserver relative-path)))
    (else
     (web-get-shared-link fileserver target-fullpath recepientid))))
