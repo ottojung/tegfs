3dba004bbde0003899968a62866033b0e803c023
Otto Jung <otto.jung@vauplace.com>
2023-01-10 23:51:29 +0100

    entry-target-fullpath: handle parentless entries

--- src/entry-target-fullpath.scm
+++ src/entry-target-fullpath.scm
@@ -19,26 +19,32 @@
 
 %use (absolute-posix-path?) "./euphrates/absolute-posix-path-q.scm"
 %use (append-posix-path) "./euphrates/append-posix-path.scm"
+%use (assq-or) "./euphrates/assq-or.scm"
 %use (path-normalize) "./euphrates/path-normalize.scm"
 %use (string-drop-n) "./euphrates/string-drop-n.scm"
 %use (a-weblink?) "./a-weblink-q.scm"
 %use (get-root) "./get-root.scm"
 %use (keyword-entry-parent-directory) "./keyword-entry-parent-directory.scm"
 %use (keyword-entry-registry-path) "./keyword-entry-registry-path.scm"
+%use (keyword-id) "./keyword-id.scm"
 %use (keyword-target) "./keyword-target.scm"
 
 (define (entry-target-fullpath entry)
   (define target-p (assoc keyword-target entry))
   (and target-p
-       (let ((target/1 (cdr target-p)))
-         (if (a-weblink? target/1) target/1
-             (let* ((target/0 (path-normalize target/1))
-                    (target (if (absolute-posix-path? target/0) (string-drop-n 1 target/0) target/0)))
-               (let* ((parent-directory-p (assq keyword-entry-parent-directory entry))
-                      (parent-directory (and parent-directory-p (cdr parent-directory-p)))
-                      (registry-p (assq keyword-entry-registry-path entry))
-                      (registry-dir (and registry-p (dirname (cdr registry-p))))
-                      (directory (or parent-directory registry-dir)))
-                 (and directory
-                      (path-normalize
-                       (append-posix-path (get-root) directory target)))))))))
+       (let ((target/1 (cdr target-p))
+             (id (assq-or keyword-id entry #f)))
+         (cond
+          ((a-weblink? target/1) target/1)
+          ((absolute-posix-path? id) id)
+          (else
+           (let* ((target/0 (path-normalize target/1))
+                  (target (if (absolute-posix-path? target/0) (string-drop-n 1 target/0) target/0)))
+             (let* ((parent-directory-p (assq keyword-entry-parent-directory entry))
+                    (parent-directory (and parent-directory-p (cdr parent-directory-p)))
+                    (registry-p (assq keyword-entry-registry-path entry))
+                    (registry-dir (and registry-p (dirname (cdr registry-p))))
+                    (directory (or parent-directory registry-dir)))
+               (and directory
+                    (path-normalize
+                     (append-posix-path (get-root) directory target))))))))))
