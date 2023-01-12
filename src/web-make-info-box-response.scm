18aa748465fc6bba6fcb0e73cc1c2e7bd9230545
Otto Jung <otto.jung@vauplace.com>
2023-01-12 20:30:30 +0100

    web: add simple templates

---
+++ src/web-make-info-box-response.scm
@@ -0,0 +1,25 @@
+;;;; Copyright (C) 2023  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU Affero General Public License as published
+;;;; by the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU Affero General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Affero General Public License
+;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+%run guile
+
+%var web::make-info-box-response
+
+%use (web::make-generic-box-response) "./web-make-generic-box-response.scm"
+
+(define (web::make-info-box-response fmt . args)
+  (apply
+   web::make-generic-box-response
+   (cons 200 (cons fmt args))))
