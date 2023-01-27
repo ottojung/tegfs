2a66b2ca15c9870ab97518354802ea9a3023d2a8
Otto Jung <otto.jung@vauplace.com>
2023-01-27 18:05:36 +0100

    plugins: exif-remover: small fixes

--- plugins/share/exif-remover.scm
+++ plugins/share/exif-remover.scm
@@ -4,22 +4,22 @@
   (unless (= 0 code)
     (throw 'plugin-initialization-failed "No exiftool program found. Please install it first if you want to use the exif-remover plugin")))
 
-(lambda (config root plugins entry generic-fullpath)
-  (define preview?
-    (string-prefix? generic-fullpath (string-append root "/cache")))
+(lambda (config root entry generic-fullpath)
+  (define preview-prefix (string-append root "/cache"))
+  (define preview? (string-prefix? preview-prefix generic-fullpath))
   (and (not preview?)
        (or
-        (string-suffix ".jpg" generic-fullpath)
-        (string-suffix ".jpeg" generic-fullpath)
-        (string-suffix ".png" generic-fullpath)
-        (string-suffix ".gif" generic-fullpath))
+        (string-suffix? ".jpg" generic-fullpath)
+        (string-suffix? ".jpeg" generic-fullpath)
+        (string-suffix? ".png" generic-fullpath)
+        (string-suffix? ".gif" generic-fullpath))
        (let ()
          (define id (cdr (assq 'id entry)))
          (define path-to-removed
            (string-append root "/temporary/noexif-" id))
          (if (file-exists? path-to-removed)
              path-to-removed
-             (let ((status (system* (string-append "exiftool" "-all=" "-o" path-to-removed root))))
+             (let ((status (system* "exiftool" "-all=" "-o" path-to-removed root)))
                (define code (status:exit-val status))
                (unless (= 0 code)
                  (throw 'plugin-failed "Exiftool exited with non-zero code, which means that it failed."))
