cf2132a48805df84024ba2dc710a217829562b2e
Otto Jung <otto.jung@vauplace.com>
2023-01-30 20:57:28 +0100

    add fileunknown.svg

--- src/tegfs/web-server-handle.scm
+++ src/tegfs/web-server-handle.scm
@@ -28,6 +28,7 @@
     :use-module ((tegfs web-directory-svg) :select (web::directory.svg))
     :use-module ((tegfs web-directory) :select (web::directory))
     :use-module ((tegfs web-error-svg) :select (web::error.svg))
+    :use-module ((tegfs web-fileunknown-svg) :select (web::fileunknown.svg))
     :use-module ((tegfs web-full) :select (web::full))
     :use-module ((tegfs web-home) :select (web::home))
     :use-module ((tegfs web-icon) :select (web::favicon.ico))
@@ -80,6 +81,7 @@
     (/static/error.svg ,web::error.svg)
     (/static/what.svg ,web::what.svg)
     (/static/directory.svg ,web::directory.svg)
+    (/static/fileunknown.svg ,web::fileunknown.svg)
     ))
 
 (define handlers-funcmap
