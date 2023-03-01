6608b6af8fefabdd42c71e07c328f37a21c1aa68
Otto Jung <otto.jung@vauplace.com>
2023-03-01 23:37:36 +0100

    web: dont show login button when authorization is disabled

--- src/tegfs/web-display-header.scm
+++ src/tegfs/web-display-header.scm
@@ -17,17 +17,20 @@
  (guile
   (define-module (tegfs web-display-header)
     :export (web::display-header)
-    :use-module ((tegfs web-callcontext) :select (callcontext-headers callcontext-token))
+    :use-module ((tegfs web-callcontext) :select (callcontext-headers))
     :use-module ((tegfs web-get-cookie) :select (web::get-cookie))
+    :use-module ((tegfs web-token-override-p) :select (web::token-override/p))
     :use-module ((tegfs web-user-loged-in-q) :select (web::user-loged-in?))
     )))
 
 
 
 (define (web::display-header callctx)
-  (define token (callcontext-token callctx))
+  (define authoriation-disabled?
+    (web::token-override/p))
   (define loged-in?
-    (web::user-loged-in? callctx))
+    (or authoriation-disabled?
+        (web::user-loged-in? callctx)))
 
   (define headers (callcontext-headers callctx))
   (define usertype
@@ -36,11 +39,6 @@
      ((web::get-cookie "pwdtoken" headers) 'Admin)
      (else #f)))
 
-  (define logout-link
-    "<a class='highlighted last' href='logout'><li>Logout</li></a>")
-  (define login-link
-    "<a class='highlighted last' href='login'><li>Login</li></a>")
-
   (display
    "<header>
     <nav>
@@ -48,31 +46,42 @@
         <a href='home'><img src='static/logo-gray.jpeg'/></a>
         <a class='highlighted first' href='query'><li>Search</li></a>
         <a class='highlighted' href='upload'><li>Upload</li></a>
-        <a class='highlighted' href='tags'><li>Tags</li></a>
-    ")
+        <a class='highlighted")
 
-  (display
-   (if loged-in?
-       (case usertype
-         ((Admin Anonymous)
-          logout-link)
-         (else
-          login-link))
-       login-link))
+  (when authoriation-disabled?
+    (display " last"))
+
+  (display "' href='tags'><li>Tags</li></a>\n")
+
+  (define logout-link
+    "<a class='highlighted last' href='logout'><li>Logout</li></a>")
+  (define login-link
+    "<a class='highlighted last' href='login'><li>Login</li></a>")
+
+  (unless authoriation-disabled?
+    (display
+     (if loged-in?
+         (case usertype
+           ((Admin Anonymous)
+            logout-link)
+           (else
+            login-link))
+         login-link)))
 
   (display "\n</ul>\n")
 
-  (case usertype
-    ((Admin Anonymous)
-     (display "
+  (unless authoriation-disabled?
+    (case usertype
+      ((Admin Anonymous)
+       (display "
       <div id='lst'>
         <div>")
-     (if loged-in?
-         (begin
-           (display "(Logged in as ")
-           (display usertype)
-           (display ")"))
-         (display "(Token expired)"))
-     (display "</div></div>")))
+       (if loged-in?
+           (begin
+             (display "(Logged in as ")
+             (display usertype)
+             (display ")"))
+           (display "(Token expired)"))
+       (display "</div></div>"))))
 
   (display "\n</nav></header>\n"))
