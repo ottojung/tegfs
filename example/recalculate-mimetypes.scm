16b2627cb895cf987285917e0ec49a4a17fe01b5
Otto Jung <otto.jung@vauplace.com>
2023-03-01 22:00:12 +0100

    simplify examples

--- example/recalculate-mimetypes.scm
+++ example/recalculate-mimetypes.scm
@@ -1,18 +1,3 @@
-;;;; Copyright (C) 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 ;; This is an example that recalculates all entries' mimetypes.
 
 (cond-expand
@@ -28,8 +13,7 @@
 (entries-map!
  (lambda (registry-path entry)
    (define fullpath
-     (entry-target-fullpath
-      (cons (cons keyword-entry-registry-path registry-path) entry)))
+     (entry-target-fullpath entry))
    (if (not fullpath) entry
        (let ((new-mimetype (get-file-mimetype fullpath)))
          (assoc-set-value keyword-mimetype new-mimetype entry)))))
