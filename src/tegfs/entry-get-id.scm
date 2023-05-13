46aef4a5f9cd90eb309042b4721c293c7ed9877e
Otto Jung <otto.jung@vauplace.com>
2023-05-13 13:23:50 -0700

    add entry-get-id

---
+++ src/tegfs/entry-get-id.scm
@@ -0,0 +1,30 @@
+;;;; Copyright (C) 2023  Otto Jung
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU Affero General Public License as published
+;;;; by the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU Affero General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Affero General Public License
+;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(cond-expand
+ (guile
+  (define-module (tegfs entry-get-id)
+    :export (entry-get-id)
+    :use-module ((euphrates assq-or) :select (assq-or))
+    :use-module ((euphrates raisu) :select (raisu))
+    :use-module ((tegfs entry-get-target) :select (entry-get-target))
+    :use-module ((tegfs keyword-id) :select (keyword-id))
+    )))
+
+(define (entry-get-id entry)
+  (unless (or (pair? entry) (null? entry))
+    (raisu 'entry-is-not-a-list entry))
+
+  (assq-or keyword-id entry #f))
