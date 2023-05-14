a1da3477f2304378975e816c44d79befd44ca6e0
Otto Jung <otto.jung@vauplace.com>
2023-05-13 22:58:49 -0700

    test: fix config format

--- test/make-example-config.sh
+++ test/make-example-config.sh
@@ -3,7 +3,7 @@
 PASS="$(printf '%s' pass1 | sha256sum | cut '-d ' -f 1 | tr -d '\n')"
 
 printf '' > "$TEST_ROOT/config.tegfs.lisp"
-printf '(users ((pass "%s")))\n' "$PASS" >> "$TEST_ROOT/config.tegfs.lisp"
+printf '(users (((pass "%s"))))\n' "$PASS" >> "$TEST_ROOT/config.tegfs.lisp"
 printf '(fileserver "http://localhost:8082/")\n' >> "$TEST_ROOT/config.tegfs.lisp"
 printf '(sharedir "/tmp/tegfs-share")\n' >> "$TEST_ROOT/config.tegfs.lisp"
 printf '(port 8081)\n' >> "$TEST_ROOT/config.tegfs.lisp"
