502aa23b5648a9ee6eb323da787dbe2ed8a9ba56
Otto Jung <otto.jung@vauplace.com>
2023-05-14 17:41:17 -0700

    docs: disable book theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,5 +24,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+# html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('custom.css')
+    app.add_css_file('_static/custom.css')
+    app.add_css_file('customddd.css')
