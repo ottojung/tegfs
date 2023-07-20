b1cbadbbaf95cb98fa5e67b334b0888edcd2f9ce
Otto Jung <otto.jung@vauplace.com>
2023-07-19 21:34:31 -0700

    add the run-tests script

---
+++ scripts/run-tests.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+set -e
+
+case "$CI" in
+    1)
+        FILES="$(ls tests/test-* || true) $(ls tests/citest-* || true)"
+        ;;
+    *)
+        FILES=$(ls tests/test-* || true)
+        ;;
+esac
+
+TESTCOUNT=$(echo "$FILES" | wc -l)
+INDEX=0
+FAILED=0
+
+for FILE in $FILES
+do
+	INDEX=$((INDEX + 1))
+	SHORT="$(basename "$FILE")"
+
+	printf '(%s/%s)' "$INDEX" "$TESTCOUNT"
+	printf ' %s ... ' "$SHORT"
+
+	case "$FILE" in
+		*.scm|*.sld)
+			R=$(if sh "$FILE" 2>&1
+			    then printf '✓'
+			    else printf 'X'
+			    fi)
+			;;
+		*.sh)
+			R=$(if sh "$FILE" 2>&1
+			    then printf '✓'
+			    else printf 'X'
+			    fi)
+			;;
+	esac
+
+	printf '%s' "$R" | grep -v -e '^;;; ' || true
+	printf '%s' "$R" | grep -q -e '✓' || FAILED=1
+done
+
+if test "$FAILED" = 1
+then exit 1
+fi
