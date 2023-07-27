115111caa015b09099489775720c47671b3d17d1
Otto Jung <otto.jung@vauplace.com>
2023-07-26 22:25:24 -0700

    move add-entry to r7rs scheme

--- src/tegfs/add-entry.scm
+++ src/tegfs/add-entry.scm
@@ -1,57 +1,5 @@
 ;;;; Copyright (C) 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-(cond-expand
- (guile
-  (define-module (tegfs add-entry)
-    :export (add-entry)
-    :use-module ((euphrates absolute-posix-path-q) :select (absolute-posix-path?))
-    :use-module ((euphrates alphanum-lowercase-alphabet) :select (alphanum-lowercase/alphabet))
-    :use-module ((euphrates append-posix-path) :select (append-posix-path))
-    :use-module ((euphrates append-string-file) :select (append-string-file))
-    :use-module ((euphrates assoc-set-default) :select (assoc-set-default))
-    :use-module ((euphrates assoc-set-value) :select (assoc-set-value))
-    :use-module ((euphrates assq-or) :select (assq-or))
-    :use-module ((euphrates date-get-current-string) :select (date-get-current-string))
-    :use-module ((euphrates file-or-directory-exists-q) :select (file-or-directory-exists?))
-    :use-module ((euphrates list-deduplicate) :select (list-deduplicate/reverse))
-    :use-module ((euphrates make-directories) :select (make-directories))
-    :use-module ((euphrates raisu) :select (raisu))
-    :use-module ((euphrates random-choice) :select (random-choice))
-    :use-module ((euphrates read-string-file) :select (read-string-file))
-    :use-module ((euphrates string-strip) :select (string-strip))
-    :use-module ((euphrates tilda-a) :select (~a))
-    :use-module ((euphrates write-string-file) :select (write-string-file))
-    :use-module ((tegfs a-weblink-q) :select (a-weblink?))
-    :use-module ((tegfs default-db-path) :select (default-db-path))
-    :use-module ((tegfs entry-get-id) :select (entry-get-id))
-    :use-module ((tegfs entry-get-target) :select (entry-get-target))
-    :use-module ((tegfs entry-print) :select (entry-print))
-    :use-module ((tegfs get-file-mimetype) :select (get-file-mimetype))
-    :use-module ((tegfs get-registry-files) :select (get-registry-files))
-    :use-module ((tegfs get-root) :select (get-root))
-    :use-module ((tegfs get) :select (tegfs-get))
-    :use-module ((tegfs keyword-date) :select (keyword-date))
-    :use-module ((tegfs keyword-id) :select (keyword-id))
-    :use-module ((tegfs keyword-mimetype) :select (keyword-mimetype))
-    :use-module ((tegfs keyword-prev) :select (keyword-prev))
-    :use-module ((tegfs keyword-tags) :select (keyword-tags))
-    :use-module ((tegfs last-id-filename) :select (last-id-filename))
-    )))
-
-
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define (add-entry entry0)
 
---
+++ src/tegfs/add-entry.sld
@@ -0,0 +1,99 @@
+
+(define-library
+  (tegfs add-entry)
+  (export add-entry)
+  (import
+    (only (euphrates absolute-posix-path-q)
+          absolute-posix-path?))
+  (import
+    (only (euphrates alphanum-lowercase-alphabet)
+          alphanum-lowercase/alphabet))
+  (import
+    (only (euphrates append-posix-path)
+          append-posix-path))
+  (import
+    (only (euphrates append-string-file)
+          append-string-file))
+  (import
+    (only (euphrates assoc-set-default)
+          assoc-set-default))
+  (import
+    (only (euphrates assoc-set-value)
+          assoc-set-value))
+  (import (only (euphrates assq-or) assq-or))
+  (import
+    (only (euphrates date-get-current-string)
+          date-get-current-string))
+  (import
+    (only (euphrates file-or-directory-exists-q)
+          file-or-directory-exists?))
+  (import
+    (only (euphrates list-deduplicate)
+          list-deduplicate/reverse))
+  (import
+    (only (euphrates make-directories)
+          make-directories))
+  (import (only (euphrates raisu) raisu))
+  (import
+    (only (euphrates random-choice) random-choice))
+  (import
+    (only (euphrates read-string-file)
+          read-string-file))
+  (import
+    (only (euphrates string-strip) string-strip))
+  (import (only (euphrates tilda-a) ~a))
+  (import
+    (only (euphrates write-string-file)
+          write-string-file))
+  (import (only (tegfs a-weblink-q) a-weblink?))
+  (import
+    (only (tegfs default-db-path) default-db-path))
+  (import (only (tegfs entry-get-id) entry-get-id))
+  (import
+    (only (tegfs entry-get-target) entry-get-target))
+  (import (only (tegfs entry-print) entry-print))
+  (import
+    (only (tegfs get-file-mimetype)
+          get-file-mimetype))
+  (import
+    (only (tegfs get-registry-files)
+          get-registry-files))
+  (import (only (tegfs get-root) get-root))
+  (import (only (tegfs get) tegfs-get))
+  (import (only (tegfs keyword-date) keyword-date))
+  (import (only (tegfs keyword-id) keyword-id))
+  (import
+    (only (tegfs keyword-mimetype) keyword-mimetype))
+  (import (only (tegfs keyword-prev) keyword-prev))
+  (import (only (tegfs keyword-tags) keyword-tags))
+  (import
+    (only (tegfs last-id-filename) last-id-filename))
+  (import
+    (only (scheme base)
+          _
+          and
+          assq
+          begin
+          car
+          cond
+          cond-expand
+          define
+          else
+          equal?
+          if
+          lambda
+          let
+          list->string
+          map
+          newline
+          not
+          or
+          quote
+          string->symbol
+          symbol?
+          unless
+          when))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin (include-from-path "tegfs/add-entry.scm")))
+    (else (include "add-entry.scm"))))
