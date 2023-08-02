2da4e008bf46f36966b67e41711e5f84024503d8
Otto Jung <otto.jung@vauplace.com>
2023-08-01 23:04:07 -0700

    categorization-completion: move to r7rs

--- src/tegfs/categorization-complete-selection-cont.scm
+++ src/tegfs/categorization-complete-selection-cont.scm
@@ -1,34 +1,5 @@
 ;;;; Copyright (C) 2022, 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-(cond-expand
- (guile
-  (define-module (tegfs categorization-complete-selection-cont)
-    :export (categorization-complete-selection/cont)
-    :use-module ((euphrates list-and-map) :select (list-and-map))
-    :use-module ((euphrates list-deduplicate) :select (list-deduplicate/reverse))
-    :use-module ((euphrates list-get-duplicates) :select (list-get-duplicates))
-    :use-module ((euphrates negate) :select (negate))
-    :use-module ((tegfs categorization-to-prolog-full) :select (categorization->prolog/full))
-    :use-module ((tegfs categorization-translate-choices) :select (categorization-translate-choices))
-    :use-module ((tegfs parse-tag) :select (parse-tag))
-    :use-module ((tegfs profun-compute-ground) :select (profun-compute-ground))
-    :use-module ((tegfs type-tag-huh) :select (type-tag?))
-    :use-module ((tegfs unparse-tag) :select (unparse-tag))
-    :use-module ((tegfs unstar-symbol) :select (unstar-symbol))
-    )))
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define (categorization-complete-selection/cont ast/flatten all-tags starred)
   (define-values (translated-tree ambiguous-branches)
---
+++ src/tegfs/categorization-complete-selection-cont.sld
@@ -0,0 +1,56 @@
+
+(define-library
+  (tegfs categorization-complete-selection-cont)
+  (export categorization-complete-selection/cont)
+  (import
+    (only (euphrates list-and-map) list-and-map))
+  (import
+    (only (euphrates list-deduplicate)
+          list-deduplicate/reverse))
+  (import
+    (only (euphrates list-get-duplicates)
+          list-get-duplicates))
+  (import (only (euphrates negate) negate))
+  (import
+    (only (tegfs categorization-to-prolog-full)
+          categorization->prolog/full))
+  (import
+    (only (tegfs categorization-translate-choices)
+          categorization-translate-choices))
+  (import (only (tegfs parse-tag) parse-tag))
+  (import
+    (only (tegfs profun-compute-ground)
+          profun-compute-ground))
+  (import (only (tegfs type-tag-huh) type-tag?))
+  (import (only (tegfs unparse-tag) unparse-tag))
+  (import
+    (only (tegfs unstar-symbol) unstar-symbol))
+  (import
+    (only (scheme base)
+          and
+          append
+          begin
+          car
+          cdr
+          cons
+          define
+          define-values
+          if
+          lambda
+          list
+          map
+          member
+          not
+          null?
+          quasiquote
+          quote))
+  (cond-expand
+    (guile (import (only (srfi srfi-1) filter)))
+    (else (import (only (srfi 1) filter))))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "tegfs/categorization-complete-selection-cont.scm")))
+    (else (include
+            "categorization-complete-selection-cont.scm"))))
