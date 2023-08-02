1582ce26aa4da5f89c0f405a7f2490f4c56c14e3
Otto Jung <otto.jung@vauplace.com>
2023-08-01 23:01:59 -0700

    categorization-completion: remove old code

--- src/tegfs/categorization-complete-selection-cont.scm
+++ src/tegfs/categorization-complete-selection-cont.scm
@@ -17,21 +17,11 @@
  (guile
   (define-module (tegfs categorization-complete-selection-cont)
     :export (categorization-complete-selection/cont)
-    :use-module ((euphrates comp) :select (comp))
-    :use-module ((euphrates const) :select (const))
-    :use-module ((euphrates curry-if) :select (curry-if))
-    :use-module ((euphrates fn) :select (fn))
-    :use-module ((euphrates identity) :select (identity))
     :use-module ((euphrates list-and-map) :select (list-and-map))
-    :use-module ((euphrates list-deduplicate) :select (list-deduplicate list-deduplicate/reverse))
-    :use-module ((euphrates list-find-first) :select (list-find-first))
+    :use-module ((euphrates list-deduplicate) :select (list-deduplicate/reverse))
     :use-module ((euphrates list-get-duplicates) :select (list-get-duplicates))
-    :use-module ((euphrates list-singleton-q) :select (list-singleton?))
-    :use-module ((euphrates multiset) :select (list->multiset multiset->list multiset-filter))
     :use-module ((euphrates negate) :select (negate))
-    :use-module ((euphrates raisu) :select (raisu))
     :use-module ((euphrates tilda-a) :select (~a))
-    :use-module ((tegfs categorization-starred-symbol-huh) :select (categorization-starred-symbol?))
     :use-module ((tegfs categorization-to-prolog-full) :select (categorization->prolog/full))
     :use-module ((tegfs categorization-translate-choices) :select (categorization-translate-choices))
     :use-module ((tegfs parse-tag) :select (parse-tag))
@@ -44,54 +34,6 @@
   (define s (~a x))
   (or (string-prefix? "<" s)
       (string-suffix? ">" s)))
-
-;; transitive over type-symbols, but not a general transitive closure
-(define (get-parents ast/flatten tag/starred)
-  (let loop ((tag/starred tag/starred) (buf '()))
-    (when (member tag/starred buf)
-      (raisu 'cycle-detected! tag/starred buf))
-
-    (apply
-     append
-     (filter
-      identity
-      (map
-       (lambda (production)
-         (and (member tag/starred (cdr production))
-              (let ((immediate (car production)))
-                (if (type-symbol? immediate)
-                    (loop immediate (cons tag/starred buf))
-                    (list immediate)))))
-       ast/flatten)))))
-
-;; transitive over symbols that have single parent
-(define (get-parents/transitive/reflexive ast/flatten ast/flatten/unstarred closed tag/starred)
-  (define (get initial)
-    (let loop ((tag/starred initial) (ast ast/flatten) (buf '()))
-      (when (member tag/starred buf)
-        (raisu 'cycle-detected! tag/starred buf))
-
-      (let* ((immediate-parents
-              (filter
-               identity
-               (map
-                (lambda (production)
-                  (and (member tag/starred (cdr production))
-                       (car production)))
-                ast)))
-             (parent/starred
-              (if (list-singleton? immediate-parents)
-                  (car immediate-parents)
-                  (list-find-first (fn member % closed) #f immediate-parents))))
-        (if parent/starred
-            (let ((parent (unstar-symbol parent/starred)))
-              (cons parent (loop parent ast/flatten/unstarred (cons tag/starred buf))))
-            '()))))
-
-  (define ret
-    ((curry-if null? (const (get (unstar-symbol tag/starred))))
-     (get tag/starred)))
-  (cons tag/starred ret))
 
 (define (categorization-complete-selection/cont ast/flatten all-tags starred)
   (define-values (translated-tree ambiguous-branches)
@@ -140,83 +82,3 @@
    (if (null? duplicates) '()
        (list (cons 'duplicates duplicates)))
    ))
-
-(define (categorization-complete-selection/cont/old ast/flatten all-tags starred)
-  (define starred/unstarred
-    (map unstar-symbol starred))
-
-  (define starred-productions
-    (filter categorization-starred-symbol? (map car ast/flatten)))
-
-  (define starred-non-productions
-    (apply
-     append
-     (map (lambda (production)
-            (filter categorization-starred-symbol? (cdr production)))
-          ast/flatten)))
-
-  (define ast/flatten/unstarred
-    (map (lambda (production)
-           (cons
-            (unstar-symbol (car production))
-            (list-deduplicate
-             (map unstar-symbol (cdr production)))))
-         ast/flatten))
-
-  (define main-production
-    (car (car ast/flatten)))
-
-  (define (closure closed tag/starred)
-    (get-parents/transitive/reflexive ast/flatten ast/flatten/unstarred closed tag/starred))
-
-  (define transitive-closures
-    (map (comp (closure '())) starred))
-
-  (define closed
-    (list-deduplicate
-     (map unstar-symbol
-          (apply append
-                 (filter
-                  (comp (member main-production))
-                  transitive-closures)))))
-
-  (define (ambiguous? tag/starred)
-    (define cl (closure closed tag/starred))
-    (not (member main-production cl)))
-
-  (define ambiguous
-    (filter ambiguous? starred))
-
-  (define ok-tags
-    (filter
-     (negate type-symbol?)
-     (filter
-      (negate (comp (equal? main-production)))
-      (list-deduplicate
-       (map unstar-symbol (apply append transitive-closures))))))
-
-  (define ambiguous-tags
-    (and (not (null? ambiguous))
-         (map
-          (lambda (amb)
-            (define unstarred (unstar-symbol amb))
-            (cons unstarred
-                  (map unstar-symbol (get-parents ast/flatten/unstarred unstarred))))
-          ambiguous)))
-
-  (define duplicates
-    (let* ((S (list->multiset starred/unstarred))
-           (S* (multiset-filter
-                S (lambda (key value) (> value 1))))
-           (L (multiset->list S*)))
-      (and (not (null? L))
-           L)))
-
-  (append
-   `((ok . ,ok-tags))
-   (if ambiguous-tags
-       (list (cons 'ambiguous ambiguous-tags))
-       (list))
-   (if duplicates
-       (list (cons 'duplicates duplicates))
-       (list))))
