31fda591af6a022654e5d13480ea5129182b6f8e
Otto Jung <otto.jung@vauplace.com>
2023-08-09 15:02:22 -0700

    move web-collectgarbage-nocall to r7rs

--- src/tegfs/web-collectgarbage-nocall.scm
+++ src/tegfs/web-collectgarbage-nocall.scm
@@ -1,41 +1,5 @@
 ;;;; Copyright (C) 2022, 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-(cond-expand
- (guile
-  (define-module (tegfs web-collectgarbage-nocall)
-    :export (web::collectgarbage/nocall)
-    :use-module ((euphrates directory-files) :select (directory-files))
-    :use-module ((euphrates file-delete) :select (file-delete))
-    :use-module ((euphrates hashmap) :select (hashmap-delete! hashmap-foreach))
-    :use-module ((euphrates path-without-extension) :select (path-without-extension))
-    :use-module ((euphrates raisu) :select (raisu))
-    :use-module ((tegfs current-time-p) :select (current-time/p))
-    :use-module ((tegfs custom-tempentry-still-valid-huh) :select (custom-tempentry-still-valid?))
-    :use-module ((tegfs custom-tempentry) :select (custom-tempentry?))
-    :use-module ((tegfs filemap) :select (filemap-delete-by-recepientid! filemap-ref-by-recepientid))
-    :use-module ((tegfs log-info) :select (log-info))
-    :use-module ((tegfs log-warning) :select (log-warning))
-    :use-module ((tegfs permission-still-valid-huh) :select (permission-still-valid?))
-    :use-module ((tegfs permission) :select (permission-filemap permission?))
-    :use-module ((tegfs sharedinfo) :select (sharedinfo-date sharedinfo-recepientid sharedinfo-stime sharedinfo?))
-    :use-module ((tegfs sharereceipt) :select (sharereceipt?))
-    :use-module ((tegfs webcore-context) :select (context-filemap/2 context-sharedir context-tempentries))
-    )))
-
-
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define sharedinfo-time-left
   (case-lambda
---
+++ src/tegfs/web-collectgarbage-nocall.sld
@@ -0,0 +1,85 @@
+
+(define-library
+  (tegfs web-collectgarbage-nocall)
+  (export web::collectgarbage/nocall)
+  (import
+    (only (euphrates directory-files)
+          directory-files))
+  (import
+    (only (euphrates file-delete) file-delete))
+  (import
+    (only (euphrates hashmap)
+          hashmap-delete!
+          hashmap-foreach))
+  (import
+    (only (euphrates path-without-extension)
+          path-without-extension))
+  (import (only (euphrates raisu) raisu))
+  (import
+    (only (tegfs current-time-p) current-time/p))
+  (import
+    (only (tegfs custom-tempentry-still-valid-huh)
+          custom-tempentry-still-valid?))
+  (import
+    (only (tegfs custom-tempentry) custom-tempentry?))
+  (import
+    (only (tegfs filemap)
+          filemap-delete-by-recepientid!
+          filemap-ref-by-recepientid))
+  (import (only (tegfs log-info) log-info))
+  (import (only (tegfs log-warning) log-warning))
+  (import
+    (only (tegfs permission-still-valid-huh)
+          permission-still-valid?))
+  (import
+    (only (tegfs permission)
+          permission-filemap
+          permission?))
+  (import
+    (only (tegfs sharedinfo)
+          sharedinfo-date
+          sharedinfo-recepientid
+          sharedinfo-stime
+          sharedinfo?))
+  (import
+    (only (tegfs sharereceipt) sharereceipt?))
+  (import
+    (only (tegfs webcore-context)
+          context-filemap/2
+          context-sharedir
+          context-tempentries))
+  (import
+    (only (scheme base)
+          +
+          -
+          <
+          _
+          begin
+          cadr
+          car
+          cond
+          cons
+          define
+          define-syntax
+          else
+          for-each
+          if
+          lambda
+          let
+          let*
+          max
+          or
+          quote
+          set!
+          syntax-rules
+          unless))
+  (import (only (scheme case-lambda) case-lambda))
+  (cond-expand
+    (guile (import (only (srfi srfi-18) current-time)))
+    (else (import (only (srfi 18) current-time))))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "tegfs/web-collectgarbage-nocall.scm")))
+    (else (include "web-collectgarbage-nocall.scm"))))
