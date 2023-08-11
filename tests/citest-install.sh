3c001eba9556003bedaf29cebae02b1ab05b9e5b
Otto Jung <otto.jung@vauplace.com>
2023-08-10 21:38:48 -0700

    test: dont assume sudo exists

--- tests/citest-install.sh
+++ tests/citest-install.sh
@@ -2,5 +2,10 @@
 
 . tests/common.sh
 
-sudo make --silent install
+if command -v sudo 1>/dev/null 2>/dev/null
+then MAYBESUDO=sudo
+else MAYBESUDO=""
+fi
+
+$MAYBESUDO make --silent install
 tegfs --version 1>/dev/null
