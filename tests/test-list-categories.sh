a948f952f38560ab8fad5e75a0594efb83e7a106
Otto Jung <otto.jung@vauplace.com>
2023-08-11 16:39:24 -0700

    tests: add list-categories case

---
+++ tests/test-list-categories.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+. tests/common.sh
+
+RESULT=$($TEST_FS --quiet categorization list-categories)
+case "$RESULT" in
+    "audio video image text song recording clip lecture photo meme drawing selfie book pasta") ;;
+    *)
+        printf "Expected different entries, got:\n%s" "$RESULT" 1>&2
+        exit 1
+        ;;
+esac
