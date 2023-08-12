8ec20cb6c659bec2d818e2f178c8d398f7aadb02
Otto Jung <otto.jung@vauplace.com>
2023-08-12 15:32:00 -0700

    move download-to-temporary-file.scm to r7rs

--- src/tegfs/download-to-temporary-file.scm
+++ src/tegfs/download-to-temporary-file.scm
@@ -1,31 +1,5 @@
 ;;;; Copyright (C) 2022, 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-(cond-expand
- (guile
-  (define-module (tegfs download-to-temporary-file)
-    :export (download-to-temporary-file)
-    :use-module ((euphrates dprintln) :select (dprintln))
-    :use-module ((euphrates tilda-s) :select (~s))
-    :use-module ((euphrates url-get-hostname-and-port) :select (url-get-hostname-and-port))
-    :use-module ((euphrates url-get-protocol) :select (url-get-protocol))
-    :use-module ((tegfs fatal) :select (fatal))
-    :use-module ((tegfs make-temporary-filename-local) :select (make-temporary-filename/local))
-    )))
-
-
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define (download-to-temporary-file url)
   (dprintln "Downloading...")
@@ -33,7 +7,7 @@
          ;; NOTE: some websites (looking at you 8chan) require referer to be set to its domain name, which is silly!! and which is stupid >:
          (home (string-append (url-get-protocol url) "://" (url-get-hostname-and-port url)))
          (headers (string-append "referer: " (~s home))))
-    (unless (= 0 (status:exit-val (system* "wget" url "-O" target)))
-      (unless (= 0 (status:exit-val (system* "wget" "--header" headers url "-O" target)))
+    (unless (= 0 (system*/exit-code "wget" url "-O" target))
+      (unless (= 0 (system*/exit-code "wget" "--header" headers url "-O" target))
         (fatal "Could not download ~s" url)))
     target))
---
+++ src/tegfs/download-to-temporary-file.sld
@@ -0,0 +1,33 @@
+
+(define-library
+  (tegfs download-to-temporary-file)
+  (export download-to-temporary-file)
+  (import (only (euphrates dprintln) dprintln))
+  (import
+    (only (euphrates system-star-exit-code)
+          system*/exit-code))
+  (import (only (euphrates tilda-s) ~s))
+  (import
+    (only (euphrates url-get-hostname-and-port)
+          url-get-hostname-and-port))
+  (import
+    (only (euphrates url-get-protocol)
+          url-get-protocol))
+  (import (only (tegfs fatal) fatal))
+  (import
+    (only (tegfs make-temporary-filename-local)
+          make-temporary-filename/local))
+  (import
+    (only (scheme base)
+          =
+          begin
+          define
+          let*
+          string-append
+          unless))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path
+               "tegfs/download-to-temporary-file.scm")))
+    (else (include "download-to-temporary-file.scm"))))
