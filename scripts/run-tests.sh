6510450f778df7ce41e04722f9acfc656ac05932
Otto Jung <otto.jung@vauplace.com>
2023-08-12 17:04:14 -0700

    tests: improve shell output

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -39,22 +39,30 @@ do
 	printf ' %s ... ' "$SHORT"
 
 	case "$FILE" in
+
+		*.sh)
+			if sh "$FILE" 2>"dist/testlog-$INDEX.txt" 1>"dist/testlog-$INDEX.txt"
+			then printf '✓'
+			else
+				printf 'X\n'
+				printf ' ----------------\n'
+				cat "dist/testlog-$INDEX.txt"
+				printf ' ----------------\n'
+				FAILED=1
+			fi
+			printf '\n'
+			;;
+
 		*.sld)
 			R=$(if guile --r7rs -L "src" -L "tests" -s "$FILE" 2>&1
 			    then printf '✓'
 			    else printf 'X'
 			    fi)
+			printf '%s' "$R" | grep -v -e '^;;; ' || true
+			printf '%s' "$R" | grep -q -e '✓' || FAILED=1
 			;;
-		*.sh)
-			R=$(if sh "$FILE" 2>&1
-			    then printf '✓'
-			    else printf 'X'
-			    fi)
-			;;
-	esac
 
-	printf '%s' "$R" | grep -v -e '^;;; ' || true
-	printf '%s' "$R" | grep -q -e '✓' || FAILED=1
+	esac
 done
 
 if test "$FAILED" = 1
