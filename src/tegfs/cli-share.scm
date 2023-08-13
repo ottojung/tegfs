555aeb84bd9fbc997c1261ea39691ab5a757bc0f
Otto Jung <otto.jung@vauplace.com>
2023-08-12 21:53:08 -0700

    move cli-share.scm to r7rs

--- src/tegfs/cli-share.scm
+++ src/tegfs/cli-share.scm
@@ -1,43 +1,5 @@
 ;;;; Copyright (C) 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-(cond-expand
- (guile
-  (define-module (tegfs cli-share)
-    :export (CLI::share CLI::share/parse)
-    :use-module ((euphrates assq-or) :select (assq-or))
-    :use-module ((euphrates list-and-map) :select (list-and-map))
-    :use-module ((euphrates list-singleton-q) :select (list-singleton?))
-    :use-module ((euphrates read-string-file) :select (read-string-file))
-    :use-module ((euphrates run-syncproc-re-star) :select (run-syncproc/re*))
-    :use-module ((euphrates string-to-seconds) :select (string->seconds))
-    :use-module ((euphrates stringf) :select (stringf))
-    :use-module ((euphrates tilda-a) :select (~a))
-    :use-module ((euphrates un-tilda-s) :select (un~s))
-    :use-module ((tegfs a-weblink-q) :select (a-weblink?))
-    :use-module ((tegfs config-get) :select (config-get))
-    :use-module ((tegfs fatal) :select (fatal))
-    :use-module ((tegfs get-config) :select (get-config))
-    :use-module ((tegfs keyword-config-fileserver) :select (keyword-config-fileserver))
-    :use-module ((tegfs keyword-config-port) :select (keyword-config-port))
-    :use-module ((tegfs web-current-fileserver-p) :select (web::current-fileserver/p))
-    :use-module ((tegfs web-default-port) :select (web::default-port))
-    :use-module ((tegfs web-get-server-operator-key-file) :select (web::get-server-operator-key-file))
-    :use-module ((tegfs web-get-target-link) :select (web::get-target-link))
-    )
-  ))
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define (CLI::share <share-duration> <entry-id>)
   (define config (get-config))
---
+++ src/tegfs/cli-share.sld
@@ -0,0 +1,66 @@
+
+(define-library
+  (tegfs cli-share)
+  (export CLI::share CLI::share/parse)
+  (import (only (euphrates assq-or) assq-or))
+  (import
+    (only (euphrates list-and-map) list-and-map))
+  (import
+    (only (euphrates list-singleton-q)
+          list-singleton?))
+  (import
+    (only (euphrates read-string-file)
+          read-string-file))
+  (import
+    (only (euphrates run-syncproc-re-star)
+          run-syncproc/re*))
+  (import
+    (only (euphrates string-to-seconds)
+          string->seconds))
+  (import (only (euphrates stringf) stringf))
+  (import (only (euphrates tilda-a) ~a))
+  (import (only (euphrates un-tilda-s) un~s))
+  (import (only (tegfs a-weblink-q) a-weblink?))
+  (import (only (tegfs config-get) config-get))
+  (import (only (tegfs fatal) fatal))
+  (import (only (tegfs get-config) get-config))
+  (import
+    (only (tegfs keyword-config-fileserver)
+          keyword-config-fileserver))
+  (import
+    (only (tegfs keyword-config-port)
+          keyword-config-port))
+  (import
+    (only (tegfs web-current-fileserver-p)
+          web::current-fileserver/p))
+  (import
+    (only (tegfs web-default-port) web::default-port))
+  (import
+    (only (tegfs web-get-server-operator-key-file)
+          web::get-server-operator-key-file))
+  (import
+    (only (tegfs web-get-target-link)
+          web::get-target-link))
+  (import
+    (only (scheme base)
+          =
+          and
+          begin
+          car
+          define
+          define-values
+          if
+          list
+          list?
+          newline
+          null?
+          pair?
+          parameterize
+          quote
+          unless
+          when))
+  (import (only (scheme write) display))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin (include-from-path "tegfs/cli-share.scm")))
+    (else (include "cli-share.scm"))))
