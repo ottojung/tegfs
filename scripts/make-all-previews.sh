d14b72f7393f1bc6f08a8257c237a57efe5154da
Otto Jung <otto.jung@vauplace.com>
2023-08-17 21:43:29 -0700

    scripts: make preview maker more verbose

--- scripts/make-all-previews.sh
+++ scripts/make-all-previews.sh
@@ -3,8 +3,19 @@
 TEGFS=tegfs
 test -n "$1" && TEGFS="$1"
 
+echo "Making previews..."
+echo "Querying tegfs..."
+
+FIRST=1
+
 $TEGFS query --dirpreview --format '%F "#tegfs-separator#" %P' | grep -v '//NA//' | while IFS= read -r FILE
 do
+	if test $FIRST = 1
+	then
+		echo "Got resulting entries."
+		FIRST=0
+	fi
+
 	PREVIEW="$(echo "$FILE" | awk -F '#tegfs-separator#' '{ print $2 }')"
 	test -f "$PREVIEW" && continue
 	test -e "$TEGFS_ROOT/cache/failed-previews/$PREVIEW" && continue
