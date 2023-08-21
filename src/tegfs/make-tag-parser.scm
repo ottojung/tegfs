e51110aa61b2e32ac4366b15dcaada8331cf7b87
Otto Jung <otto.jung@vauplace.com>
2023-08-20 20:54:34 -0700

    make-tag-parser: move to r7rs

--- src/tegfs/make-tag-parser.scm
+++ src/tegfs/make-tag-parser.scm
@@ -1,35 +1,5 @@
 ;;;; Copyright (C) 2022, 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU Affero General Public License as published
-;;;; by the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU Affero General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU Affero General Public License
-;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-(cond-expand
- (guile
-  (define-module (tegfs make-tag-parser)
-    :export (make-tag-parser make-tag-structure-parser)
-    :use-module ((euphrates comp) :select (comp))
-    :use-module ((euphrates list-intersperse) :select (list-intersperse))
-    :use-module ((euphrates list-or-map) :select (list-or-map))
-    :use-module ((euphrates list-split-on) :select (list-split-on))
-    :use-module ((euphrates raisu) :select (raisu))
-    :use-module ((euphrates stringf) :select (stringf))
-    :use-module ((euphrates tilda-a) :select (~a))
-    :use-module ((tegfs tag-structure-sep1) :select (tag-structure-sep1))
-    :use-module ((tegfs tag-structure-sep2) :select (tag-structure-sep2))
-    :use-module ((tegfs tags-this-variable) :select (tags-this-variable/string))
-    )))
-
-
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (define (make-tag-structure-parser/chars counter)
   (lambda (tag)
---
+++ src/tegfs/make-tag-parser.sld
@@ -0,0 +1,61 @@
+
+(define-library
+  (tegfs make-tag-parser)
+  (export
+    make-tag-parser
+    make-tag-structure-parser)
+  (import (only (euphrates comp) comp))
+  (import
+    (only (euphrates list-intersperse)
+          list-intersperse))
+  (import
+    (only (euphrates list-or-map) list-or-map))
+  (import
+    (only (euphrates list-split-on) list-split-on))
+  (import (only (euphrates raisu) raisu))
+  (import (only (euphrates stringf) stringf))
+  (import (only (euphrates tilda-a) ~a))
+  (import
+    (only (tegfs tag-structure-sep1)
+          tag-structure-sep1))
+  (import
+    (only (tegfs tag-structure-sep2)
+          tag-structure-sep2))
+  (import
+    (only (tegfs tags-this-variable)
+          tags-this-variable/string))
+  (import
+    (only (scheme base)
+          append
+          apply
+          begin
+          cadr
+          car
+          case
+          cdr
+          cond
+          cons
+          define
+          else
+          equal?
+          lambda
+          let*
+          list->string
+          map
+          not
+          null?
+          quasiquote
+          quote
+          string->list
+          string->symbol
+          unquote
+          unquote-splicing
+          when))
+  (cond-expand
+    (guile (import (only (srfi srfi-13) string-null?)))
+    (else (import (only (srfi 13) string-null?))))
+  (cond-expand
+    (guile (import (only (guile) include-from-path))
+           (begin
+             (include-from-path "tegfs/make-tag-parser.scm")))
+    (else (include "make-tag-parser.scm"))))
