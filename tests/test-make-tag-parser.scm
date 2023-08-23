6170791fb998d5c8a93a4841268ff5b1a7965e00
Otto Jung <otto.jung@vauplace.com>
2023-08-22 21:00:26 -0700

    tests: improve the make-tag-parser code

--- tests/test-make-tag-parser.scm
+++ tests/test-make-tag-parser.scm
@@ -1,58 +1,48 @@
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "$")) (parser 'video)))
+(define (test1 input expected-output)
+  (define parser (make-tag-parser 7))
+  (assert= expected-output (parser input)))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X")) (parser 'video=X)))
+(define (test-error input error-type)
+  (define parser (make-tag-parser 4))
+  (assert-throw
+   'type-error
+   (parser input)))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X" "Y")) (parser 'video=X,Y)))
+(test1 'video `((video "$")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X" "Y" "Z" "W")) (parser 'video=X,Y,Z,W)))
+(test1 'video=X '((video "X")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "$")) (parser 'video=$)))
+(test1 'video=X,Y '((video "X" "Y")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "$" "Y")) (parser 'video=$,Y)))
+(test1 'video=X,Y,Z,W '((video "X" "Y" "Z" "W")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X" "$")) (parser 'video=X,$)))
+(test1 'video=$ '((video "$")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X" "$" "Y")) (parser 'video=X,$,Y)))
+(test1 'video=$,Y '((video "$" "Y")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X" "Y") (video "Z" "W")) (parser 'video=X,Y=Z,W)))
+(test1 'video=X,$ '((video "X" "$")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X" "Y") (video "Z" "W")) (parser 'video=X,Y=Z,W)))
+(test1 'video=X,$,Y '((video "X" "$" "Y")))
 
-(let ((parser (make-tag-parser 7)))
-  (assert= '((video "X") (video "X")) (parser 'video=X=X)))
+(test1 'video=X,Y=Z,W '((video "X" "Y") (video "Z" "W")))
 
-(assert-throw
- 'type-error
- (let ((parser (make-tag-parser 7)))
-   (parser 'video=)))
+(test1 'video=X,Y=Z,W '((video "X" "Y") (video "Z" "W")))
 
-(assert-throw
- 'type-error
- (let ((parser (make-tag-parser 7)))
-   (parser 'video=X=)))
+(test1 'video=X=X '((video "X") (video "X")))
 
-(assert-throw
- 'type-error
- (let ((parser (make-tag-parser 7)))
-   (parser 'video==)))
+(test-error 'video= 'type-error)
+(test-error 'video=X= 'type-error)
+(test-error 'video== 'type-error)
+(test-error 'video==X 'type-error)
+(test-error '=X 'type-error)
+(test-error '=X,Y 'type-error)
+(test-error '=X=Y 'type-error)
+(test-error 'video=X, 'type-error)
+(test-error 'video=, 'type-error)
+(test-error 'video,, 'type-error)
+(test-error 'video,= 'type-error)
 
-(assert-throw
- 'type-error
- (let ((parser (make-tag-parser 7)))
-   (parser 'video==X)))
-
-(assert-throw
- 'type-error
- (let ((parser (make-tag-parser 7)))
-   (parser '=X)))
+(test1 "video" `((video "$")))
+(test1 "video=X" '((video "X")))
+(test1 "video=X,Y" '((video "X" "Y")))
--- tests/test-make-tag-parser.sld
+++ tests/test-make-tag-parser.sld
@@ -6,7 +6,15 @@
     (only (euphrates assert-throw) assert-throw))
   (import
     (only (tegfs make-tag-parser) make-tag-parser))
-  (import (only (scheme base) begin let quote))
+  (import
+    (only (scheme base)
+          begin
+          define
+          quasiquote
+          quote))
+  (cond-expand
+    (guile (import (only (srfi srfi-64) test-error)))
+    (else (import (only (srfi 64) test-error))))
   (cond-expand
     (guile (import (only (guile) include-from-path))
            (begin
